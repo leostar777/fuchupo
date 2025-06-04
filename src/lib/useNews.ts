4642c839020aca2f23df34b63f93120e722f16a6
someno katsuyuki <somek206@gmail.com>
2025-06-04 21:44:10 +0900

    Update useNews.ts

--- src/lib/useNews.ts
+++ src/lib/useNews.ts
@@ -8,11 +8,17 @@ export interface Article {
 
 export function useNews() {
   const [articles, setArticles] = useState<Article[]>([]);
+
   useEffect(() => {
     fetch(import.meta.env.BASE_URL + "news.json", { cache: "no-store" })
       .then((r) => r.json())
-      .then(setArticles)
+      .then((data: Article[]) => {
+        // pubDateの新しい順にソート
+        const sortedData = data.sort((a, b) => new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime());
+        setArticles(sortedData);
+      })
       .catch(console.error);
   }, []);
+
   return articles;
 }
